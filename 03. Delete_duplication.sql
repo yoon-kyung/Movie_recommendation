fe45eddd0b26ac4567604b3ea3ea902fd145c245
Yooonkyung <iamoyk58@gmail.com>
2017-11-01 13:21:36 +0900

    Modify sql

--- 03. Delete_duplication.sql
+++ 03. Delete_duplication.sql
@@ -1,31 +1,23 @@
 SHOW DATABASES;
 USE movie_review;
-SHOW tables;
+SHOW TABLES;
 
-# raw data의 행 갯수 확인
-SELECT count(*) From review_data;
-# 결과값 : 26038
+# raw_file row 갯수 확인
+SELECT COUNT(*) FROM raw_file;
+# 결과값 : 140021
 
 # 중복된 데이터 확인
-SELECT DISTINCT `UserID`, `Movie name`, `Score` FROM review_data;
-SELECT COUNT(DISTINCT `UserID`, `Movie name`, `Score`) FROM review_data;
-# 결과값 : 19176
+DESC raw_file;
+SELECT DISTINCT `user`, `title`, `genre`, `score` FROM raw_file;
+SELECT COUNT(DISTINCT `user`, `title`, `genre`, `score`) FROM raw_file;
+# 결과값 : 90152
 
 # 중복된 데이터가 제거된 새로운 테이블 생성
-CREATE TABLE tmp 
-	SELECT DISTINCT `UserID`, `Movie name`, `Score` 
-    FROM review_data;
-    
-# 새로운 테이블 tmp가 생성 되었는지 확인
+CREATE TABLE data_set
+		SELECT DISTINCT `user`, `title`, `genre`, `score` FROM raw_file;
+        
+# 새로운 테이블 data_set이 생성되었는지 확인
 SHOW TABLES;
-DESC tmp;
-SELECT count(*) FROM tmp;
-
-# 테이블명 변경
-# 1. review_data -> raw_data
-# 2. tmp -> reviews
-RENAME TABLE review_data TO raw_data;
-RENAME TABLE tmp TO reviews;
-
-# 테이블명 바뀌었는지 확인
-SHOW TABLES;
+DESC data_set;
+SELECT COUNT(*) FROM data_set;
+# 결과값 : 90152
